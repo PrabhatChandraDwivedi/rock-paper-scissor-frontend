efc538a4aa1a424c8acc645936135bf219360acf
PRABHAT CHANDRA DWIVEDI <mr.prabhatdwivedi@gmail.com>
2024-03-16 22:00:22 +0530

    updating app.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
-import './App.css';
-import React, { useEffect, useState } from "react";
-import axios from 'axios';
-import io from 'socket.io-client';
-function App() {
-  // const [roomName,setRoomName] = useState("");
-  const socket = io('http://localhost:5000')
-  const createRoom =()=>{
-    let roomName = (btoa(Math.random()).toUpperCase().replace(/=/g, '').substring(10,19));
-    // setRoomName(roomName);
-    localStorage.setItem("roomName",roomName);
-    console.log(roomName)
-    socket.emit('create-room',roomName);
-  }
-  
-  const joinRoom = ()=>{
-    console.log("hello")
-    let roomName = localStorage.getItem("roomName");
-    socket.emit('join-room',roomName);
-  }
+import "./App.css";
+import React from "react";
+import Home from "./Components/Home/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-//   useEffect(() => {
-//     axios.get("http://localhost:5000/",{ mode: 'no-cors'}).then((response) => {
-//       console.log(response)
-//   })
-// },[])
-  
+
+function App() {
   return (
- <div>
-  <button onClick={createRoom}>Create</button>
-  <input></input>
-  <button onClick={joinRoom}>Join</button>
- </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
