7d279ca811fa7a8922f388b57bf0243af0e919ef
PRABHAT CHANDRA DWIVEDI <mr.prabhatdwivedi@gmail.com>
2024-07-23 20:34:43 +0530

    adding render deployment

--- src/Components/Socket/Socket.js
+++ src/Components/Socket/Socket.js
@@ -1,3 +1,4 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000");
+// export const socket = io("http://localhost:5000");
+export const socket = io("https://rock-paper-scissor-backend-cidh.onrender.com");
